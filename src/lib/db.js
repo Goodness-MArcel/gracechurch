61d9fae130d92a67301606bef2e3ae2aa4b0b666
gudnex <marcelgoodness144@gmail.com>
2026-01-17 22:23:05 +0100

    new

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,11 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
+// Force Postgres driver dependencies to be included in server bundles (Vercel/Turbopack).
+// Sequelize dynamically requires these, which can be missed by output tracing.
+require('pg');
+require('pg-hstore');
+
 const { sequelize, connectDB, disconnectDB } = require('./db-commonjs.js');
 const db = require('../../models');
 
